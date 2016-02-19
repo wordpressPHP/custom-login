9493612df65f5f34b044262f5706e60a94959db1
Austin Passy <austin@passy.co>
2016-02-19 07:58:09 -0800

    Update Customize controller classes.

--- includes/customize/class-cl-customize-alpha-color-control.php
+++ includes/customize/class-cl-customize-alpha-color-control.php
@@ -5,13 +5,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-//class CL_Customize_Color_Alpha_Control extends WP_Customize_Color_Control {
-class CL_Customize_Color_Alpha_Control extends WP_Customize_Control {
+/**
+ * Class CL_Customize_Alpha_Color_Control
+ */
+class CL_Customize_Alpha_Color_Control extends WP_Customize_Color_Control {
 
+	/**
+	 * @access public
+	 * @var string
+	 */
 	public $type = 'alphacolor';
-	//public $palette = '#3FADD7,#555555,#666666, #F5f5f5,#333333,#404040,#2B4267';
 	public $palette = true;
-	public $default = '#3FADD7';
+	public $default = 'rgba(0,0,0,100)';
 
 	protected function render() {
 		$id    = 'customize-control-' . str_replace( '[', '-', str_replace( ']', '', $this->id ) );
@@ -19,14 +24,17 @@ class CL_Customize_Color_Alpha_Control extends WP_Customize_Control {
 		<li id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class ); ?>">
 			<?php $this->render_content(); ?>
 		</li>
-	<?php }
+		<?php
+	}
 
-	public function render_content() { ?>
+	public function render_content() {
+		?>
 		<label>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 			<input type="text" data-palette="<?php echo $this->palette; ?>"
 			       data-default-color="<?php echo $this->default; ?>" value="<?php echo intval( $this->value() ); ?>"
-			       class="pluto-color-control" <?php $this->link(); ?> />
+			       class="cl-alpha-color-control" <?php $this->link(); ?> />
 		</label>
-	<?php }
+		<?php
+	}
 }
